e6d504972c171f86a4e83d27ab27fcb83782270c
avinashcbsharma <156292691+avinashcbsharma@users.noreply.github.com>
2024-02-22 11:15:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+    function updateTimer() {
+      // Get the current date and time
+      var currentDateTime = new Date();      
+      var formattedDateTime = currentDateTime.toLocaleString();   
+      document.getElementById("timer").innerText = formattedDateTime;
+    }
+  
+    setInterval(updateTimer, 1000);
+    updateTimer()
